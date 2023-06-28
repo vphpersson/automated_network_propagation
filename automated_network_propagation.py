ed39b330d6e8f1ed1adaada464430157c76b87cc
vph <vph.persson@gmail.com>
2023-06-28 18:22:22 +0200

    Add better logging.

--- automated_network_propagation.py
+++ automated_network_propagation.py
@@ -28,95 +28,106 @@ class ConnectionData:
 
 
 async def feed(request: Request):
+    try:
+        if isinstance(request._transport_peername, (list, tuple)):
+            client_ip, client_port = request._transport_peername
+        elif request._transport_peername is not None:
+            client_ip = str(request._transport_peername)
+            client_port = None
+        else:
+            client_ip = None
+            client_port = None
 
-    if isinstance(request._transport_peername, (list, tuple)):
-        client_ip, client_port = request._transport_peername
-    elif request._transport_peername is not None:
-        client_ip = str(request._transport_peername)
-        client_port = None
-    else:
-        client_ip = None
-        client_port = None
+        if subscription_value := request.rel_url.query.get('subscriptions'):
+            subscriptions = tuple(set(subscription_value.split(',')))
+        else:
+            subscriptions = None
 
-
-    if subscription_value  := request.rel_url.query.get('subscriptions'):
-        subscriptions = tuple(set(subscription_value.split(',')))
-    else:
-        subscriptions = None
-
-    connection_data = ConnectionData(
-        queue=Queue(),
-        client_ip=client_ip,
-        client_port=client_port,
-        subscriptions=subscriptions
-    )
-
-    request.app.connections.add(connection_data)
-    LOG.info(
-        msg='A client was added to the connections',
-        extra=dict(
+        connection_data = ConnectionData(
+            queue=Queue(),
             client_ip=client_ip,
             client_port=client_port,
             subscriptions=subscriptions
         )
-    )
 
-    with suppress(CancelledError, ConnectionResetError):
-        response: EventSourceResponse
-        async with sse_response(request=request) as response:
-            data_tuple: tuple[str, str]
-            while data_tuple := await connection_data.queue.get():
-                event, data = data_tuple
-                await response.send(data=data, event=event)
+        request.app.connections.add(connection_data)
+        LOG.info(
+            msg='A client was added to the connections',
+            extra=dict(
+                client_ip=client_ip,
+                client_port=client_port,
+                subscriptions=subscriptions
+            )
+        )
 
-    # NOTE: Not convinced by this.
-    return response
+        with suppress(CancelledError, ConnectionResetError):
+            response: EventSourceResponse
+            async with sse_response(request=request) as response:
+                data_tuple: tuple[str, str]
+                while data_tuple := await connection_data.queue.get():
+                    event, data = data_tuple
+                    await response.send(data=data, event=event)
 
+        # NOTE: Not convinced by this.
+        return response
+    except:
+        LOG.exception(msg='An unexpected error occurred in feed.')
+        return Response(status=500)
 
 async def submit(request: Request):
+    try:
+        data: dict[str, ...] = await request.json()
+        connection_data: ConnectionData
 
-    data: dict[str, ...] = await request.json()
-    connection_data: ConnectionData
+        if '@timestamp' in data:
+            if 'source' in data and 'ip' in data['source']:
+                ip_address = data['source']['ip']
+            elif 'client' in data and 'ip' in data['client']:
+                ip_address = data['client']['ip']
+            else:
+                # TODO: What to include in `extra`?
+                # TODO: Do mapping; `rule` fields, unique WAF id.
+                LOG.warning(msg='No IP address is presented in a submitted alert document.')
+                return Response(status=400)
 
-    if '@timestamp' in data:
-        if 'source' in data and 'ip' in data['source']:
-            ip_address = data['source']['ip']
-        elif 'client' in data and 'ip' in data['client']:
-            ip_address = data['client']['ip']
+            for connection_data in request.app.connections:
+                if connection_data.subscriptions is None or 'block' in connection_data.subscriptions:
+                    await connection_data.queue.put(('block', ip_address))
+                    LOG.info(
+                        msg='An IP address was was queued',
+                        extra=dict(
+                            type='block',
+                            client_ip=connection_data.client_ip,
+                            client_port=connection_data.client_port
+                        )
+                    )
+            return Response()
+        elif 'type' in data:
+            for connection_data in request.app.connections:
+                data_type: str = data['type']
+                if connection_data.subscriptions is None or data_type in connection_data.subscriptions:
+                    await connection_data.queue.put((data_type, json_dumps(data)))
+                    LOG.info(
+                        msg='Network data was queued',
+                        extra=dict(
+                            type=data_type,
+                            client_ip=connection_data.client_ip,
+                            client_port=connection_data.client_port
+                        )
+                    )
+            return Response()
         else:
-            # TODO: What to include in `extra`?
-            # TODO: Do mapping; `rule` fields, unique WAF id.
-            LOG.warning(msg='No IP address is presented in a submitted alert document.')
+            LOG.warning(
+                msg='The submitted data is not in the expected format.',
+                extra=dict(
+                    error=dict(input=await request.text()),
+                    _ecs_logger_handler_options=dict(merge_extra=True)
+                )
+            )
             return Response(status=400)
-
-        for connection_data in request.app.connections:
-            if connection_data.subscriptions is None or 'block' in connection_data.subscriptions:
-                await connection_data.queue.put(('block', ip_address))
-                LOG.info(
-                    msg='An IP address was was queued',
-                    extra=dict(
-                        type='block',
-                        client_ip=connection_data.client_ip,
-                        client_port=connection_data.client_port
-                    )
-                )
-        return Response()
-    elif 'type' in data:
-        for connection_data in request.app.connections:
-            data_type: str = data['type']
-            if connection_data.subscriptions is None or data_type in connection_data.subscriptions:
-                await connection_data.queue.put((data_type, json_dumps(data)))
-                LOG.info(
-                    msg='Network data was queued',
-                    extra=dict(
-                        type=data_type,
-                        client_ip=connection_data.client_ip,
-                        client_port=connection_data.client_port
-                    )
-                )
-        return Response()
-    else:
-        return Response(status=400)
+    except:
+        LOG.exception(msg='An unexpected error occurred in submit.')
+        return Response(status=500)
 
 
 def main():
